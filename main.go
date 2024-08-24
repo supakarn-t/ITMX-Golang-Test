8972abc72edc3c766c903686725d6c6a478bcb55
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-08-24 23:01:43 +0700

    UpdateCustomer function

--- main.go
+++ main.go
@@ -116,6 +116,44 @@ func GetCustomer(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 }
 
 func UpdateCustomer(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/customers/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var customer Customer
+	err = db.First(&customer, id).Error
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Not found customer with id %d", id), http.StatusNotFound)
+		return
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&customer)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if customer.Name == "" || customer.Age <= 0 {
+		http.Error(w, "Invalid customer data", http.StatusBadRequest)
+		return
+	}
+
+	err = db.Save(&customer).Error
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err = json.NewEncoder(w).Encode(customer)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func DeleteCustomer(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
